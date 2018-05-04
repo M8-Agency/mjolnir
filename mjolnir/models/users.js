1ddf5e8689312de5a0ad7b87836b616f86d2bf6c
Carlos Cordoba <carloscba1@gmail.com>
2018-05-04 10:37:37 -0300

    modelo usuarios

--- mjolnir/models/users.js
+++ mjolnir/models/users.js
@@ -7,24 +7,65 @@ module.exports = function setupUserModel(config){
     const sequelize = db(config)
 
     return sequelize.define('user', {
-        username : {
-            type: Sequelize.STRING, 
-            allowNull: true,
-        },
-        name : {
-            type: Sequelize.STRING, 
+        firstname : {
+            type: Sequelize.STRING.STRING(64), 
             allowNull: false,
         },
         lastname : {
-            type: Sequelize.STRING, 
+            type: Sequelize.STRING.STRING(64), 
             allowNull: false,
         },
         email : {
-            type: Sequelize.STRING, 
+            type: Sequelize.STRING.STRING(64), 
             allowNull: false,
             validate: {
                 isEmail: true
             }            
-        }
+        },
+        pic : {
+            type: Sequelize.STRING, 
+            allowNull: true,
+            validate: {
+                isUrl: true
+            }                         
+        },                
+        username : {
+            type: Sequelize.STRING.STRING(64), 
+            allowNull: false,
+        },
+        gender : {
+            type: Sequelize.STRING(1), 
+            allowNull: false,
+            validate: {
+                isIn: [['m', 'f']],
+            }
+        },        
+        address : {
+            type: Sequelize.STRING, 
+            allowNull: true,
+        },
+        zipcode : {
+            type: Sequelize.STRING.STRING(32), 
+            allowNull: true,
+        },
+        city : {
+            type: Sequelize.STRING.STRING(32), 
+            allowNull: true,
+        },
+        country : {
+            type: Sequelize.STRING.STRING(32), 
+            allowNull: true,
+        },
+        birthday : {
+            type: Sequelize.DATEONLY, 
+            allowNull: true,
+            validate: {
+                isDate: true
+            }
+        },
+        phone : {
+            type: Sequelize.STRING(64), 
+            allowNull: true,
+        }        
     })
 };
