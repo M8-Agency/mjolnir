4034224a07223fee2ca68c459e3dd17627d6711c
Ernesto del Frade <ernestodelfrade@Ernestos-MacBook-Pro.local>
2019-01-29 15:58:26 -0500

    changing migration

--- db/migrate.js
+++ db/migrate.js
@@ -1,34 +1,46 @@
-require('dotenv').config();
-const dbConfig = require('./config');
-const Sequelize = require('sequelize');
-const Confirm = require('prompt-confirm');
+require("dotenv").config();
+const dbConfig = require("./config");
+const Sequelize = require("sequelize");
+const Confirm = require("prompt-confirm");
 
-db = new Sequelize(dbConfig)
+db = new Sequelize(dbConfig);
 
-const applicationsModel = require('../modules/applications/applications.model')(db);
-const usersModel = require('../modules/users/users.model')(db);
-const eventsModel = require('../modules/events/events.model')(db);
-const actionsModel = require('../modules/actions/actions.model')(db);
-const emailsModel = require('../modules/emails/emails.model')(db);
+UserAction = db.define("user_action", {
+  picture: Sequelize.STRING,
+  valid_score: Sequelize.BOOLEAN
+});
 
-usersModel.hasMany(actionsModel, { as: 'userId' });
-eventsModel.hasMany(actionsModel, { as: 'eventId' });
+// const applicationsModel = require('../modules/applications/applications.model')(db);
+const usersModel = require("../modules/users/users.model")(db);
+// const eventsModel = require('../modules/events/events.model')(db);
+const actionsModel = require("../modules/actions/actions.model")(db);
+// const emailsModel = require('../modules/emails/emails.model')(db);
 
-applicationsModel.hasMany(eventsModel, { as: 'applicationId' });
-applicationsModel.hasMany(emailsModel, { as: 'applicationId2' });
+// usersModel.hasMany(actionsModel, { as: 'userId' });
+// eventsModel.hasMany(actionsModel, { as: 'eventId' });
 
-console.log(dbConfig)
+// applicationsModel.hasMany(eventsModel, { as: 'applicationId' });
+// applicationsModel.hasMany(emailsModel, { as: 'applicationId2' });
 
-const confirm = new Confirm(`Migrate ${process.env.DB_DATABASE} on ${process.env.DB_HOST}`).ask((answer) => {
-    if (answer) {
-        db.sync({
-            force: true
-        }).then(() => {
-            console.log('suceess')
-            process.exit(0)
-        }).catch((error) => {
-            console.log('error', error)
-            process.exit(1)
-        })
-    }
-})
+usersModel.belongsToMany(actionsModel, { through: UserAction });
+actionsModel.belongsToMany(usersModel, { through: UserAction });
+
+console.log(dbConfig);
+
+const confirm = new Confirm(
+  `Migrate ${process.env.DB_DATABASE} on ${process.env.DB_HOST}`
+).ask(answer => {
+  if (answer) {
+    db.sync({
+      force: true
+    })
+      .then(() => {
+        console.log("suceess");
+        process.exit(0);
+      })
+      .catch(error => {
+        console.log("error", error);
+        process.exit(1);
+      });
+  }
+});
