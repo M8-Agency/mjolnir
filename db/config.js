6dfebe43a7e508daf83dc4c4fce85de5a7cca8f4
Ernesto del Frade <ernestodelfrade@Ernestos-MacBook-Pro.local>
2019-01-31 15:22:44 -0500

    setting server and database timezone

--- db/config.js
+++ db/config.js
@@ -1,22 +1,23 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-    database : process.env.DB_DATABASE,
-    username : process.env.DB_USERNAME,
-    password : process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    setup : true,
-    ssl: true,
-    dialectOptions: {
-        ssl: {
-            require: true
-        }
-    },
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }    
-}
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  dialect: "postgres",
+  timezone: "-05:00",
+  setup: true,
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      require: true
+    }
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
