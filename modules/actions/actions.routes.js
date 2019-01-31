d3500b1a99fb3b440be68daad4377cf9b83c0f98
Ernesto del Frade <ernestodelfrade@Ernestos-MacBook-Pro.local>
2019-01-31 11:56:50 -0500

    adjusting action validation

--- modules/actions/actions.routes.js
+++ modules/actions/actions.routes.js
@@ -1,40 +1,42 @@
-const Joi = require('joi');
-module.exports = (db) => {
-    const actionsControllers = require('./actions.controller')(db)
-    return [
-        {
-            path: '/api/actions',
-            method: 'GET',
-            handler: actionsControllers.find,
-        },
-        {
-            path: '/api/actions/points',
-            method: 'GET',
-            handler: actionsControllers.points,
-        },        
-        {
-            path: '/api/actions/{code}',
-            method: 'GET',
-            handler: actionsControllers.findCode,
-        },        
-        {
-            path: '/api/actions',
-            method: 'POST',
-            handler: actionsControllers.create,
-            config: {
-                validate : {
-                    /*
-                    payload : Joi.object().keys({
-                        code : Joi.string().required()
-                    }),
-                    */
-                    query : Joi.object().keys({
-                        token : Joi.string().required()
-                    }),
-                }
-            }            
-        },
-        
-    ]
-}
+const Joi = require("joi");
+module.exports = db => {
+  const actionsControllers = require("./actions.controller")(db);
+  return [
+    {
+      path: "/api/actions",
+      method: "GET",
+      handler: actionsControllers.find
+    },
+    {
+      path: "/api/actions/points",
+      method: "GET",
+      handler: actionsControllers.points
+    },
+    {
+      path: "/api/actions/{code}",
+      method: "GET",
+      handler: actionsControllers.findCode
+    },
+    {
+      path: "/api/actions/create-static",
+      method: "POST",
+      handler: actionsControllers.createaction
+    },
+    {
+      path: "/api/actions",
+      method: "POST",
+      handler: actionsControllers.create,
+      config: {
+        validate: {
+          payload: Joi.object().keys({
+            token: Joi.string().required()
+          })
 
+          // query: Joi.object().keys({
+          //   token: Joi.string().required()
+          // })
+        }
+      }
+    }
+  ];
+};
