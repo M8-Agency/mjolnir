045a9fc6bff4b0b9ce7b371cf3fce32c4e7e13e9
Ernesto del Frade <ernestodelfrade@Ernestos-MacBook-Pro.local>
2019-01-31 11:38:16 -0500

    disabling unused modules

--- server.js
+++ server.js
@@ -1,58 +1,59 @@
-const hapi = require('hapi');
-const Sequelize = require('sequelize')
-const dbConfig = require('./db/config')
-db = new Sequelize(dbConfig)
+const hapi = require("hapi");
+const Sequelize = require("sequelize");
+const dbConfig = require("./db/config");
+db = new Sequelize(dbConfig);
 
 const server = new hapi.Server({
-    host: '0.0.0.0',
-    port: process.env.PORT || 3000,
-    routes: {
-        cors: {
-            origin: ['*'],
-        }
+  host: "0.0.0.0",
+  port: process.env.PORT || 3000,
+  routes: {
+    cors: {
+      origin: ["*"]
     }
+  }
 });
 
 const init = async () => {
-    await server.register([
-        {
-            plugin: require('./modules/auth/auth.module'),
-            options: {
-                db
-            }
-        },
-        {
-            plugin: require('./modules/actions/actions.module'),
-            options: {
-                db
-            }
-        },
-        {
-            plugin: require('./modules/applications/applications.module'),
-            options: {
-                db
-            }
-        },
-        {
-            plugin: require('./modules/users/users.module'),
-            options: {
-                db
-            }
-        },
-        {
-            plugin: require('./modules/events/events.module'),
-            options: {
-                db
-            }
-        },
-        {
-            plugin: require('./modules/trivias/trivias.module'),
-            options: {
-                db
-            }
-        }    ])
-    await server.start()
-    console.log(`Server runnig at ${server.info.port}`)
-}
+  await server.register([
+    {
+      plugin: require("./modules/auth/auth.module"),
+      options: {
+        db
+      }
+    },
+    {
+      plugin: require("./modules/actions/actions.module"),
+      options: {
+        db
+      }
+    },
+    // {
+    //     plugin: require('./modules/applications/applications.module'),
+    //     options: {
+    //         db
+    //     }
+    // },
+    {
+      plugin: require("./modules/users/users.module"),
+      options: {
+        db
+      }
+    }
+    // {
+    //   plugin: require("./modules/events/events.module"),
+    //   options: {
+    //     db
+    //   }
+    // }
+    // {
+    //   plugin: require("./modules/trivias/trivias.module"),
+    //   options: {
+    //     db
+    //   }
+    // }
+  ]);
+  await server.start();
+  console.log(`Server runnig at ${server.info.port}`);
+};
 
 init();
