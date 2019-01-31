5db6555d544b54accd3016f29723494da6b01bde
Ernesto del Frade <ernestodelfrade@Ernestos-MacBook-Pro.local>
2019-01-31 11:53:43 -0500

    commenting email signin

--- modules/auth/auth.routes.js
+++ modules/auth/auth.routes.js
@@ -1,22 +1,21 @@
-const Joi = require('joi');
-module.exports = (db) => {
-    const authControllers = require('./auth.controller')(db)
-    return [
-        {
-            path: '/api/auth/signup',
-            method: 'POST',
-            handler: authControllers.signup
-        },
-        {
-            path: '/api/auth/signin',
-            method: 'POST',
-            handler: authControllers.signin
-        },
-        {
-            path: '/api/auth/signinemail',
-            method: 'POST',
-            handler: authControllers.signinemail
-        }         
-    ]
-}
-
+const Joi = require("joi");
+module.exports = db => {
+  const authControllers = require("./auth.controller")(db);
+  return [
+    {
+      path: "/api/auth/signup",
+      method: "POST",
+      handler: authControllers.signup
+    },
+    {
+      path: "/api/auth/signin",
+      method: "POST",
+      handler: authControllers.signin
+    }
+    // {
+    //   path: "/api/auth/signinemail",
+    //   method: "POST",
+    //   handler: authControllers.signinemail
+    // }
+  ];
+};
