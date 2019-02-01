3d734354e76dbde061bb6aaf94b28122e825d7a3
Ernesto del Frade <ernestodelfrade@Ernestos-MacBook-Pro.local>
2019-02-01 11:25:30 -0500

    adding field to events, creating actions

--- db/migrate.js
+++ db/migrate.js
@@ -13,7 +13,8 @@ const eventsModel = db.define("user_action", {
     unique: true
   },
   picture: Sequelize.STRING,
-  valid_score: Sequelize.BOOLEAN
+  valid_score: Sequelize.BOOLEAN,
+  share_platform: Sequelize.STRING
 });
 
 // const applicationsModel = require('../modules/applications/applications.model')(db);
@@ -51,6 +52,23 @@ const confirm = new Confirm(`Migrate ${process.env.DB_DATABASE} on ${process.env
           .bulkCreate([
             { name: "Register", code: "REGISTER", description: "Register a user", points: 15, frequency: "unique", frequency_limit: 1, max_actions: 1, can_pass_limit: false },
             { name: "Upload", code: "UPLOAD", description: "Upload an image", points: 1, frequency: "daily", frequency_limit: 1, max_actions: 10, can_pass_limit: false },
+            { name: "Newsletter", code: "NEWSLETTER", description: "Newsletter subscription", points: 15, frequency: "unique", frequency_limit: 1, max_actions: 1, can_pass_limit: false },
+            { name: "Palmera Sticker", code: "PALMERA", description: "Palmera Sticker", points: 10, frequency: "daily", frequency_limit: 1, max_actions: 10, can_pass_limit: false },
+            { name: "Sol Sticker", code: "SUN", description: "Sol Sticker", points: 15, frequency: "daily", frequency_limit: 1, max_actions: 10, can_pass_limit: false },
+            { name: "Amor Sticker", code: "AMOR", description: "Amor Sticker", points: 20, frequency: "daily", frequency_limit: 1, max_actions: 10, can_pass_limit: false },
+            { name: "Bebida Sticker", code: "DRINK", description: "Bebida Sticker", points: 25, frequency: "daily", frequency_limit: 1, max_actions: 10, can_pass_limit: false },
+            { name: "Ball Sticker", code: "BALL", description: "Ball Sticker", points: 30, frequency: "daily", frequency_limit: 1, max_actions: 10, can_pass_limit: false },
+            { name: "Bucket Sticker", code: "BUCKET", description: "Bucket Sticker", points: 35, frequency: "daily", frequency_limit: 1, max_actions: 10, can_pass_limit: false },
+            { name: "Carnaval Sticker", code: "CARNAVAL", description: "Carnaval Sticker", points: 40, frequency: "daily", frequency_limit: 1, max_actions: 10, can_pass_limit: false },
+            { name: "Comida Sticker", code: "FOOD", description: "Comida Sticker", points: 45, frequency: "daily", frequency_limit: 1, max_actions: 10, can_pass_limit: false },
+            { name: "Buceo Sticker", code: "DIVE", description: "Buceo Sticker", points: 50, frequency: "daily", frequency_limit: 1, max_actions: 10, can_pass_limit: false },
+            { name: "Velero Sticker", code: "BOAT", description: "Velero Sticker", points: 55, frequency: "daily", frequency_limit: 1, max_actions: 10, can_pass_limit: false },
+            { name: "Refer a friend", code: "REFERRAL", description: "Refer a friend", points: 5, frequency: "daily", frequency_limit: 1, max_actions: 30, can_pass_limit: true },
+            { name: "Compra 1 ticket", code: "BUYONE", description: "Compra 1 ticket", points: 15, frequency: "unique", frequency_limit: 1, max_actions: 1, can_pass_limit: false },
+            { name: "Compra 2 ticket", code: "BUYTWO", description: "Compra 2 ticket", points: 20, frequency: "unique", frequency_limit: 1, max_actions: 1, can_pass_limit: false },
+            { name: "Compra 3 ticket", code: "BUYTHREE", description: "Compra 3 ticket", points: 25, frequency: "unique", frequency_limit: 1, max_actions: 1, can_pass_limit: false },
+            { name: "Compra 4 ticket", code: "BUYFOUR", description: "Compra 4 ticket", points: 30, frequency: "unique", frequency_limit: 1, max_actions: 1, can_pass_limit: false },
+            { name: "Compra 5+ ticket", code: "BUYMORE", description: "Compra 5+ ticket", points: 35, frequency: "unique", frequency_limit: 1, max_actions: 1, can_pass_limit: false },
             { name: "Share", code: "SHARE", description: "Share a post", points: 5, frequency: "daily", frequency_limit: 3, max_actions: 99999999, can_pass_limit: true }
           ])
           .then(() => {
